08061500fd277816b7250d60931a4915b1e6dc1d
anyibustos <anyipaolabustos@gmail.com>
2020-03-17 11:25:35 -0500

    GP1 Suma

--- src/HolaAlianza.java
+++ src/HolaAlianza.java
@@ -2,7 +2,9 @@
 public class HolaAlianza {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo suma ");
+		System.out.println(2+2);
+
 
 	}
 
