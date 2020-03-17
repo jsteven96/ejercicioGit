7d21e89a074621bfd71bc8096056e16494bfe4ae
Javier Steven Pérez Fernández <jsperez@asesoftware.com>
2020-03-17 11:36:59 -0500

    Agregado un saludo con fecha

--- src/HolaAlianza.java
+++ src/HolaAlianza.java
@@ -1,9 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class HolaAlianza {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
-
+		Date ahora = new Date();
+    	SimpleDateFormat formateador = new SimpleDateFormat("dd-MM-yyyy");
+    	System.out.println("Hola a las: "+formateador.format(ahora));
 	}
 
 }
