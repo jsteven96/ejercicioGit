7405dfc47619cb0c9289409b44d51060434c12f9
JairVillalba <jvillalba@asesoftware.com>
2020-03-17 11:27:07 -0500

    mi primer GP

--- src/HolaAlianza.java
+++ src/HolaAlianza.java
@@ -2,7 +2,8 @@
 public class HolaAlianza {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		boolean alianza = true;
+		System.out.println("Hola Mundo " + alianza);
 
 	}
 
